e8f4ea8e77216f1238c13f359efc2a8c9ca41564
Bokobo Shahni <shahni@bokobo.space>
2022-05-23 08:55:08 -0400

    ci: update release rules

--- release.config.js
+++ release.config.js
@@ -8,24 +8,41 @@ module.exports = {
   ],
   plugins: [
     ['@semantic-release/commit-analyzer', {
+      preset: 'conventionalcommits',
       releaseRules: [
         { breaking: true, release: 'major' },
         { revert: true, release: 'patch' },
+        { type: 'ci', release: 'patch' },
+        { type: 'deps', release: 'patch' },
+        { type: 'docs', release: 'patch' },
         { type: 'feat', release: 'minor' },
         { type: 'fix', release: 'patch' },
         { type: 'perf', release: 'patch' },
         { type: 'refactor', release: 'patch' },
         { type: 'test', release: 'patch' },
-        { type: 'ci', release: 'patch' },
-        { type: 'docs', release: 'patch' },
-        { type: 'deps', release: 'patch' },
         { scope: 'no-release', release: false }
       ]
     }],
     ['@semantic-release/exec', {
       'prepareCmd': 'bin/ci-release-version ${nextRelease.version}'
     }],
-    '@semantic-release/release-notes-generator',
+    ['@semantic-release/release-notes-generator', {
+      preset: 'conventionalcommits',
+      types: [
+        { type: 'build', section: 'Build System', hidden: true },
+        { type: 'chore', section: 'Miscellaneous Chores', hidden: true },
+        { type: 'ci', section: 'Continuous Integration', hidden: true },
+        { type: 'deps', section: 'Dependencies' },
+        { type: 'docs', section: 'Documentation' },
+        { type: 'feat', section: 'Features' },
+        { type: 'fix', section: 'Bug Fixes' },
+        { type: 'perf', section: 'Performance Improvements' },
+        { type: 'refactor', section: 'Code Refactoring', hidden: true },
+        { type: 'revert', section: 'Reverts' },
+        { type: 'style', section: 'Styles', hidden: true },
+        { type: 'test', section: 'Tests', hidden: true },
+      ]
+    }],
     ['@semantic-release/changelog', {
       changelogTitle: '# Jove Changelog'
     }],
